23fcc9c0f1e1799fc27d8688187d9a6002f96a6c
Anurag Singh <x23180013@student.ncirl.ie>
2024-03-12 14:29:59 +0000

    Updated error msg in  manage.py

--- manage.py
+++ manage.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Unable to import Django. Is it installed and "
+            "accessible on your PYTHONPATH env variable? Have you "
+            "activated a virtual env ?"
         ) from exc
     execute_from_command_line(sys.argv)
