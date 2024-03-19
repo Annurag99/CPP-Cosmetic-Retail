74baf1f9a8b4b4e117bb03465eb03741a0bec4de
EC2 Default User <ec2-user@ip-172-31-36-66.eu-west-1.compute.internal>
2024-03-19 23:30:52 +0000

    Changed file ext

--- demo/settings.py
+++ demo/settings.py
@@ -5,7 +5,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['d003a56484c942d88af2e1fc366a3f01.vfs.cloud9.eu-west-1.amazonaws.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
